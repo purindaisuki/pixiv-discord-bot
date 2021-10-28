52689ea719b03aa44f7fc8ac89225298f2fb767d
purindaisuki <good12dave@gmail.com>
2021-10-28 22:44:20 +0800

    fix commands export

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -5,13 +5,17 @@ import { latest } from "./new";
 import { recommend } from "./recommend";
 import { followed } from "./followed";
 
+const modules = [search, ranking, latest, recommend, followed];
+
 const makeTuple = <A extends any, B extends any>([a, b]: [A, B]): [A, B] => [
   a,
   b,
 ];
 
 export const commandsCollection = new Collection(
-  Object.values([search, ranking, latest, recommend, followed]).map((command) =>
+  Object.values(modules).map((command) =>
     makeTuple([command.data.name, command])
   )
 );
+
+export default modules;
